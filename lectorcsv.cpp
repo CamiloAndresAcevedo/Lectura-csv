657784bb5f664c859ea329a1b0365c21d4cf968a
Camilo Andres <Camilo.acevedo.2019@upb.edu.co>
2020-08-15 16:25:05 -0500

    Este es un programa que lee archivos en formato csv

--- lectorcsv.cpp
+++ lectorcsv.cpp
@@ -3,45 +3,45 @@
 //Inspirado lector de archivos Cesar Aceros.
 
 
-#include <fstream> //
-#include <string> //
-#include <vector> //
-#include <sstream> //
-#include <iostream> //
-using namespace std; //
+#include <fstream> // Libreria para leer y escribir desde / hacia archivos.
+#include <string> //Archivo de la biblioteca que contiene la defincion de constantes, funciones y algunas operaciones.
+#include <vector> //Libreria para operar con arreglos unidimensionales de datos
+#include <sstream> //Libreria que proporciona plantillas y tipos que permiten la interoperación entre búferes de flujo y objetos de cadena .
+#include <iostream> //Componente de la biblioteca, utilizado para operaciones de entrada / salida.
+using namespace std; //Creamos un bloque que asocie nuestras funciones
 
-void parseCSV() //
+void parseCSV() //Indicamos el tipo de datos.
 {
-    ifstream data("Book1.csv"); //
-    string line; //
-    vector<vector<string>> parsedCsv; //
-    int rowcount=0; //
-    int colcount=0; //
-    while(getline(data,line)) //
+    ifstream data("Book1.csv"); //Buscamos nuestro archivo csv para ser leido.
+    string line; // secuencia de caracteres guardados como una fila.
+    vector<vector<string>> parsedCsv; //Almacenamos nuestros datos de la secuencia de caracteres.
+    int rowcount=0; //Declaramos una variable.
+    int colcount=0; //Declaramos una variable.
+    while(getline(data,line)) //Ciclo para ejecutar nuestars instrucciones.
     {
-        stringstream lineStream(line); //
-        string cell; //
-        vector<string> parsedRow; //
-        colcount=0; //
-        while(getline(lineStream,cell,',')) //
+        stringstream lineStream(line); //Tratamos un string como un stream, eso nos permite la inserción y la extracción desde y a strings
+        string cell; //secuencia de caracteres guardados.
+        vector<string> parsedRow; //Almacenamos nuestros datos.
+        colcount=0; //Miramos la cantidad de columnas del archivo.
+        while(getline(lineStream,cell,',')) //Ciclo para ejecutar nuestars instrucciones.
         {
-            parsedRow.push_back(cell); //
-            colcount++; //
+            parsedRow.push_back(cell); // recibe como parámetro un elemento del mismo tipo del que se compone nuestro vector
+            colcount++; //Miramos la cantidad de columnas del archivo.
         }
-        rowcount++; //
-        parsedCsv.push_back(parsedRow); //
+        rowcount++; //Miramos la cantidad de filas del archivo.
+        parsedCsv.push_back(parsedRow); //recibe como parámetro un elemento del mismo tipo del que se compone nuestro vector
     }
-    for (int i=0; i<rowcount; i++){ //
-        for (int j=0; j < colcount ; j++){ //
-        cout << parsedCsv[i][j] << " -- "; //
+    for (int i=0; i<rowcount; i++){ //Ciclo de control para ejecutar nuestro bloque de columnas.
+        for (int j=0; j < colcount ; j++){ //Ciclo de control para ejecutar nuestro bloque de filas.
+        cout << parsedCsv[i][j] << " -- "; // imprimimos nuestro resultado separado por --.
         }
-        cout << '\n'; //
+        cout << '\n'; //Se imprime nuestro resultado con un salto de linea.
     }
 }
 
 
-int main() //
+int main() //Declaramos nuestra variable para iniciar la ejecucion del programa.
 {
-    parseCSV(); //
-    return 0; //
+    parseCSV(); //Analizamos nuestro archivo para determinar su estructura.
+    return 0; //Se abandona el main.
 }
